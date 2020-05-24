4bafc487cf558254f440054470130bba02ae8c7a
Riya Rohilla <44579551+riyarohilla@users.noreply.github.com>
2020-05-24 02:51:28 -0700

    array

---
+++ array/Hotel Bookings Possible.cpp
@@ -0,0 +1,23 @@
+bool Solution::hotel(vector<int> &arrive, vector<int> &depart, int k) {
+    sort(arrive.begin(),arrive.end());
+    sort(depart.begin(),depart.end());
+    int i=0,j=0,c=0;
+    while(i<arrive.size() && j<depart.size())
+    {
+        if(arrive[i]<depart[j])
+        {
+            c++;
+            i++;
+            
+        }
+        else
+        {
+            c--;
+            j++;
+        }
+        if(c>k)
+        return false;
+    }
+    return true;
+}
+    
